7efb00216428c9814d1856b6497c9aa07acfb725
fotoply <norberg.niels@gmail.com>
2019-05-22 11:33:34 +0200

    Added sensor setup

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
-import LIS2HH12 as Accel
-import SI7006A20 as TempHum
-import MPL3115A2 as Barometer
 from pysense import Pysense
 import time
 from network import WLAN
 import pycom as pyc
 import machine
 
+def log(s):
+    print(s)
+
+AP_SSID = 'SDU-VISITOR'
+AP_TIMEOUT = 5000
+
 pyc.heartbeat(False)
 
 wlan = WLAN(mode=WLAN.STA)
-
 pyc.rgbled(0xFF0000)
 nets = wlan.scan()
 for net in nets:
-    if net.ssid == 'SDU-VISITOR':
-        print('Network found!')
-        wlan.connect(net.ssid, auth=None, timeout=5000)
+    if net.ssid == AP_SSID:
+        log('Network found!')
+        wlan.connect(net.ssid, auth=None, timeout=AP_TIMEOUT)
         while not wlan.isconnected():
             machine.idle() # save power while waiting
-        print('WLAN connection succeeded!')
+        log('WLAN connection succeeded!')
         break
 
 if not wlan.isconnected():
@@ -31,3 +33,36 @@ if not wlan.isconnected():
 
 pyc.rgbled(0x0000FF)
 pysen = Pysense()
+
+def sensorFailedLoop():
+    while True:
+        pyc.rgbled(0x000000)
+        time.sleep(1)
+        pyc.rgbled(0x0000FF)
+        time.sleep(1)
+
+log("Checking for sensors")
+
+log("Checking accelerometer")
+from LIS2HH12 import LIS2HH12
+accel = LIS2HH12(pysen)
+if accel.acceleration() == None or accel.acceleration() == (None, None, None):
+    log("No accelerometer availale")
+    sensorFailedLoop()
+log("Accelerometer is available")
+log("Checking temperature and humidity sensor")
+from SI7006A20 import SI7006A20
+tempSens = SI7006A20(pysen)
+if tempSens.humidity() == None or tempSens.temperature() == None:
+    log("No temperature or humidity sensor available")
+    sensorFailedLoop()
+log("Temperature and humidity is available")
+log("Checking barometer")
+from MPL3115A2 import MPL3115A2
+barometer = MPL3115A2(pysen)
+if barometer.pressure() == None:
+    log("No barometer available")
+    sensorFailedLoop()
+log("Barometer is available")
+
+pyc.rgbled(0x00FF00)
